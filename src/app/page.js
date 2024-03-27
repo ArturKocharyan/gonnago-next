57d3efb3046355c3e05ea588efc976e1b6144e8a
ArturKocharyan <a.kocharyan1997@gmail.com>
2024-03-27 17:16:52 +0400

    redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import styles from "./page.module.css";
-
+import { redirect } from 'next/navigation'
 export default async function Home() {
+
+  redirect('/Armenia/entertainment')
 
   return (
     <div className={styles.main_container} >
       <div className={styles.chiled_container} >Hello</div>
-      
     </div>
   );
 }
