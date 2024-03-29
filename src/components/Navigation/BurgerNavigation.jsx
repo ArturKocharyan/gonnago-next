769327c9193cbdab0425b2d373d2ae2851d2262b
ArturKocharyan <a.kocharyan1997@gmail.com>
2024-03-29 11:25:13 +0400

    adding burgerNavigation

--- src/components/Navigation/BurgerNavigation.jsx
+++ src/components/Navigation/BurgerNavigation.jsx
@@ -1,8 +1,36 @@
-import React from "react";
 
-function BurgerNavigation () {
-    return(
-        <div></div>
+import React, {useState} from "react";
+import style from "./style.module.css";
+const DrawerCitiesLazy = React.lazy(() => import("./DrawerCities/DrawerCtits"));
+import DrawerLang from "./DrawerLang/DrawerLang";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
+function BurgerNavigation() {
+    const [mobailMenu, setMobailMenu] = useState(false);
+    return (
+        <>
+            <div
+                onClick={() => setMobailMenu(!mobailMenu)}
+                className={style.burgerMenu}
+            >
+                <span>{!mobailMenu ? <RxHamburgerMenu /> : <HiOutlineMenuAlt3 />}</span>
+            </div>
+            {mobailMenu === true && (
+                <div className={style.menuBody}>
+                    <div className={style.citiesMenu}>
+                        <DrawerCitiesLazy />
+                    </div>
+                    <div className={style.langMenu}>
+                        <DrawerLang />
+                    </div>
+
+                    <div className={style.singContainer}>
+                        <button>Sing In</button>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,16 @@
 "use client"
 
-import React, {useState } from "react";
+import React from "react";
 import logo from "../../assets/img/image2.svg";
 import style from "./style.module.css";
 import DropDownLang from "./DropDownLang/DropDownLang";
 import CitesDropDown from "./CitesDropDown/CitesDropDown";
-import { RxHamburgerMenu } from "react-icons/rx";
-import DrawerLang from "./DrawerLang/DrawerLang";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import DrawerSearch from "./DrawerSearch/DrawerSearch";
-const DrawerCitiesLazy = React.lazy(() => import("./DrawerCities/DrawerCtits"));
 import Image from "next/image";
 import SearchInput from "./SearchInput/SerachInput";
-
+import BurgerNavigation from "./BurgerNavigation";
 
 function Navigation() {
-  const [mobailMenu, setMobailMenu] = useState(false);
 
   return (
     <header className={style.mainContainer}>
@@ -40,26 +35,7 @@ function Navigation() {
       <span className={style.alterSearch}>
         <DrawerSearch />
       </span>
-      <div
-        onClick={() => setMobailMenu(!mobailMenu)}
-        className={style.burgerMenu}
-      >
-        <span>{!mobailMenu ? <RxHamburgerMenu /> : <HiOutlineMenuAlt3 />}</span>
-      </div>
-      {mobailMenu === true && (
-        <div className={style.menuBody}>
-          <div className={style.citiesMenu}>
-            <DrawerCitiesLazy  />
-          </div>
-          <div className={style.langMenu}>
-            <DrawerLang />
-          </div>
-
-          <div className={style.singContainer}>
-            <button>Sing In</button>
-          </div>
-        </div>
-      )}
+      <div className={style.burgerMenu} ><BurgerNavigation /></div>
     </header>
   );
 }
